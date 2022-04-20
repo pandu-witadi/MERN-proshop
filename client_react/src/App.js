a728d8d2ddd213fb0072a12c9eb3d15bd0f2cd35
pandu-witadi <pandu.witadi@gmail.com>
2022-04-20 07:49:20 +0700

    001 product [client]

---
+++ client_react/src/App.js
@@ -0,0 +1,29 @@
+//
+//
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Header />
+            <main className="py-3">
+                <Container>
+                    <Route path='/' component={HomeScreen} exact/>
+                    <Route path='/product/:id' component={ProductScreen} />
+                </Container>
+            </main>
+            <Footer />
+        </BrowserRouter>
+    )
+}
+
+export default App
